9a99b3449130603cc8835a0849ae2ef21548aa1e
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-11-21 13:31:33 +0530

    Stage 1 Activity Bar

--- webview-ui/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const react_dom_1 = require("react-dom");
-const App_1 = require("./App");
-react_dom_1.default.render(<react_1.default.StrictMode>
-    <App_1.default />
-  </react_1.default.StrictMode>, document.getElementById("root"));
-//# sourceMappingURL=index.js.map
