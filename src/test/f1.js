df270a8c28c34f62ff9226c09d7e64aaf6ee4e63
PrathamSutone <52961179+PrathamSutone@users.noreply.github.com>
2023-12-03 22:36:07 +0530

    Adding Chat Controller

---
+++ src/test/f1.js
@@ -0,0 +1,9 @@
+light: {
+    //   this color will be used in light color themes
+borderColor: darkblue
+}
+
+dark:{
+     // this color will be used in dark color themes
+borderColor: lightpink
+}
---
+++ src/test/f2.js
@@ -0,0 +1,10 @@
+light: {
+    //   this color will be used in light color themes
+borderColor: darkblue
+}
+
+hi
+dark:{
+     // this color will be used in dark color themes
+borderColor: lightpink
+}
